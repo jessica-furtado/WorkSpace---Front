4bbb773aa851a59f8301c95ca70364777db2f97b
Jéssica Furtado <jessicafurtado.adm@gmail.com>
2024-08-20 20:14:42 -0300

    Atualização para a atividade

--- aula00/src/App.jsx
+++ aula00/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Jéssica {2+2}</h1>
+      <h1>Hello World! {2+2}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
