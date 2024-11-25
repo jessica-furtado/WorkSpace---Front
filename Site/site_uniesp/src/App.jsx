bbb0420589a0c7e60e94b479c7117d0eca66f653
Jéssica Furtado <jessicafurtado.adm@gmail.com>
2024-11-25 20:52:43 -0300

    Segue Criação do Projeto - Site da Uniesp

--- Site/site_uniesp/src/App.jsx
+++ Site/site_uniesp/src/App.jsx
@@ -1,8 +1,29 @@
+import { Container, CssBaseline } from '@mui/material'
+import { Box } from '@mui/material'
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import BannerAd from './Components/BannerAd'
+import Inicial from './Pages/Inicial'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <CssBaseline/>
+      <Container maxWidth='lg' sx={{ backgroundColor:'#f7f7f7', minHeight: '100vh', padding: '16px', borderRadius: '8px' }}>
+        <Box display="flex" alignItems="center" justifyContent="space-between" p={2} sx={{ backgroundColor: '#fff', borderRadius: '8px', boxShadow: 1, }}>
+          <Navbar />
+        </Box>
+
+        <BannerAd />
+
+        <Box mt={4}>
+          <Routes>
+            <Route path="/" element={<Inicial />} />
+          </Routes>
+        </Box>
+      </Container>
+    </BrowserRouter>
   )
 }
 
